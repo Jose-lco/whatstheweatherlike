8a6db44f6353ddc1ee4a6d4794c02846450332b8
Jose-lco <ndungu642@gmail.com>
2020-05-20 22:47:01 -0700

    just fixing my errors

--- weather.js
+++ weather.js
@@ -53,8 +53,7 @@ let createCityButtons = function (cityName) {
     let searchHistory = `<button type="button" class="btn btn-light btn-block">${cityName}</button>`
     $(".search-history").append(searchHistory);
 };
-let cityArr = localStorage.cityStorage !== "" ? JSON.parse(localStorage.cityStorage) : [];
-console.log(localStorage.cityStorage !== "");
+let cityArr = (localStorage.cityStorage !== "") ? JSON.parse(localStorage.cityStorage) : [];
 let storeSearchHistory = function () {
     let chosenCity = $("#city").val().trim();
     cityArr.push(chosenCity);
